ad35d09670afc776c2eb3464ef17c1f137e029be
Michel Nekrasov <moscou@inbox.ru>
2022-12-17 01:09:45 +0300

    add Programm

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -9,3 +9,27 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+string[] array1 = new string[8] {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science"};
+string[] array2 = new string[array1.Length];
+void SecondArrayOfThree(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayOfThree(array1, array2);
+PrintArray(array2);

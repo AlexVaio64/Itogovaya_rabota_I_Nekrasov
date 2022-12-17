1e1178a45ae89d7c592e26243bd5d3afa3f909fb
Michel Nekrasov <moscou@inbox.ru>
2022-12-17 03:22:39 +0300

    add Programm and Readme

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -9,16 +9,27 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-string [] startingArray = new string[] {"1234", "1567", "-2", "computer science"};
-string[] resultArray = new string [0];
-
-foreach (var item in startingArray)
+string[] array1 = new string[] {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science"};
+string[] array2 = new string[array1.Length];
+void SecondArrayOfThree(string[] array1, string[] array2)
 {
-    if (item.Length <= 3)
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
     {
-    Array.Resize(ref resultArray, resultArray.Length + 1);
-    resultArray [resultArray.Length-1] = item;
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
     }
 }
-
-Console.WriteLine($"[{String.Join("; ", resultArray)}]");
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayOfThree(array1, array2);
+PrintArray(array2);
